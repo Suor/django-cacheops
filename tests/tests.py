baeaf4bd7928dd2e540efdadee1be4736f08ff84
Alexander Schepanovski <suor.web@gmail.com>
2012-03-03 09:29:38 +0800

    Added failing invalidation test

--- tests/tests.py
+++ tests/tests.py
@@ -11,8 +11,19 @@ class BaseTestCase(TestCase):
 
 
 class BasicTests(BaseTestCase):
+    fixtures = ['basic']
+
     def test_it_works(self):
         with self.assertNumQueries(1):
             cnt1 = Category.objects.cache().count()
             cnt2 = Category.objects.cache().count()
             self.assertEqual(cnt1, cnt2)
+
+    def test_invalidation(self):
+        post = Post.objects.cache().get(pk=1)
+        post.title += ' changed'
+        post.save()
+
+        with self.assertNumQueries(1):
+            changed_post = Post.objects.cache().get(pk=1)
+            self.assertEqual(post.title, changed_post.title)
