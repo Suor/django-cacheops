56486cbe63918a8f200cd96a35086817ae409f6e
Alexander Schepanovski <suor.web@gmail.com>
2012-03-03 08:51:17 +0800

    Add basic test

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,18 @@
-__test__ = {"doctest": """
->>> 1 + 1
-2
-"""}
+from django.test import TestCase
+
+from cacheops.conf import redis_conn
+from .models import Category, Post
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        super(BaseTestCase, self).setUp()
+        redis_conn.flushdb()
+
+
+class BasicTests(BaseTestCase):
+    def test_it_works(self):
+        with self.assertNumQueries(1):
+            cnt1 = Category.objects.cache().count()
+            cnt2 = Category.objects.cache().count()
+            self.assertEqual(cnt1, cnt2)
