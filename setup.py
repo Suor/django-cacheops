9f3b03abc4eb7f854d12ab7c5ffd7ce11460446f
Suor <suor.web@gmail.com>
2013-05-31 11:15:50 +0800

    Upped to 0.9.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-cacheops',
-    version='0.9.7',
+    version='0.9.8',
     author='Alexander Schepanovski',
     author_email='suor.web@gmail.com',
 
