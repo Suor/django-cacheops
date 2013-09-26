7166964eaecb645e39e0a71a56151734ee80eb05
Alexander Schepanovski <suor.web@gmail.com>
2013-09-26 22:00:52 +0800

    Fixed another imcompatability with Django 1.7

--- cacheops/query.py
+++ cacheops/query.py
@@ -136,6 +136,7 @@ def _stringify_query():
     from datetime import datetime, date
     from django.db.models.expressions import ExpressionNode, F
     from django.db.models.fields import Field
+    from django.db.models.fields.related import ManyToOneRel
     from django.db.models.sql.where import Constraint, WhereNode, ExtraWhere
     from django.db.models.sql import Query
     from django.db.models.sql.aggregates import Aggregate
@@ -158,6 +159,7 @@ def _stringify_query():
     attrs[Aggregate] = ('source', 'is_summary', 'col', 'extra')
     attrs[Date] = ('col', 'lookup_type')
     attrs[F] = ('name',)
+    attrs[ManyToOneRel] = ('field',)
 
     q = Query(None)
     q_keys = q.__dict__.keys()
