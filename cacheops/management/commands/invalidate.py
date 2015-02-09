3a0242d780498062c8a2191ef549e654fe23872a
Alexander Schepanovski <suor.web@gmail.com>
2015-02-09 18:56:56 +0700

    Fix Apps/AppConfig shim for Django 1.3

--- cacheops/management/commands/invalidate.py
+++ cacheops/management/commands/invalidate.py
@@ -8,27 +8,25 @@ except ImportError:
     from django.core.exceptions import ImproperlyConfigured
 
     class AppConfig(object):
-        def __init__(self, label):
-            self.label = label
+        def __init__(self, label, app):
+            self.label, self.app = label, app
 
         def get_model(self, model_name):
             model = get_model(self.label, model_name)
             if not model:
                 raise LookupError(
-                    "App '%s' doesn't have a '%s' model." % (self.app.label, model_name))
+                    "App '%s' doesn't have a '%s' model." % (self.label, model_name))
             return model
 
         def get_models(self, include_auto_created=False):
-            return get_models(self.label, include_auto_created=include_auto_created)
+            return get_models(self.app, include_auto_created=include_auto_created)
 
     class Apps(object):
         def get_app_config(self, app_label):
             try:
-                get_app(app_label)
+                return AppConfig(app_label, get_app(app_label))
             except ImproperlyConfigured as e:
                 raise LookupError(*e.args)
-            else:
-                return AppConfig(app_label)
     apps = Apps()
 
 from cacheops.invalidation import *
