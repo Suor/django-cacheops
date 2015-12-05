70e49d2df4494807bc5da923ec0db1fa0416cc4e
Alexander Schepanovski <suor.web@gmail.com>
2015-12-05 12:18:10 +0700

    Test on Python 3.5

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
 
         'Framework :: Django',
         'Environment :: Web Environment',
