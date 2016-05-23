d130957bfb039431d9b016665dcc20f1cf8eb62b
Kimmo Kiiski <kimmo@giosg.com>
2016-05-23 11:18:49 +0300

    Invalidations sends Django signals

--- cacheops/signals.py
+++ cacheops/signals.py
@@ -1,3 +1,7 @@
 import django.dispatch
 
 cache_read = django.dispatch.Signal(providing_args=["func", "hit"])
+invalidation_all = django.dispatch.Signal(providing_args=[])
+invalidation_model = django.dispatch.Signal(providing_args=[])
+invalidation_obj = django.dispatch.Signal(providing_args=["obj"])
+invalidation_dict = django.dispatch.Signal(providing_args=["obj_dict"])
