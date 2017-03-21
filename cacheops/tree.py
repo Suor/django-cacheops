43a6060c4f91dcc7cb8bc427f42b630ddfc013bb
Alexander Schepanovski <suor.web@gmail.com>
2017-03-21 20:29:48 +0700

    Optimize and clean up clean_dnf()

--- cacheops/tree.py
+++ cacheops/tree.py
@@ -107,23 +107,19 @@ def dnfs(qs):
             return result
 
     def clean_conj(conj, for_alias):
-        seen = {}
-        result = []
+        conds = {}
         for alias, attname, value, negation in conj:
             # "SOME" conds, negated conds and conds for other aliases should be stripped
             if value is not SOME and negation and alias == for_alias:
-                # Dupes with same values can be safely deduped with no resulting change in invalidation logic
-                if attname not in seen:
-                    seen[attname] = value
-                    result.append((attname, value))
-                # Conjs with duplicate fields and different values are useless, they will never cause invalidation
-                elif seen[attname] != value:
+                # Conjs with fields eq 2 different values will never cause invalidation
+                if attname in conds and conds[attname] != value:
                     return None
-        return result
+                conds[attname] = value
+        return conds.items()
 
     def clean_dnf(tree, for_alias):
         cleaned = [clean_conj(conj, for_alias) for conj in tree]
-        # Remove useless conjunctions
+        # Remove deleted conjunctions
         cleaned = [conj for conj in cleaned if conj is not None]
         # Any empty conjunction eats up the rest
         # NOTE: a more elaborate DNF reduction is not really needed,
