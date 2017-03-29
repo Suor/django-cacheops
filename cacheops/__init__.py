18cefb3b41b058ede33041732098c2e3da4d1b57
Alexander Schepanovski <suor.web@gmail.com>
2017-03-29 20:53:20 +0700

    Up to 3.1.3

--- cacheops/__init__.py
+++ cacheops/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (3, 1, 2)
+VERSION = (3, 1, 3)
 __version__ = '.'.join(map(str, VERSION if VERSION[-1] else VERSION[:2]))
 
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = open('README.rst').read()    \
 
 setup(
     name='django-cacheops',
-    version='3.1.2',
+    version='3.1.3',
     author='Alexander Schepanovski',
     author_email='suor.web@gmail.com',
 
