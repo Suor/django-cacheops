37eeb874d16c2727c6fea0563786cde3dcf756e8
David Fuentes <davidfb@allenta.com>
2018-09-17 17:39:03 +0200

    Use funcy.omit() to build Sentinel configuration

--- cacheops/redis.py
+++ cacheops/redis.py
@@ -95,7 +95,7 @@ def redis_client():
 
         sentinel = Sentinel(
             settings.CACHEOPS_SENTINEL['locations'],
-            omit(settings.CACHEOPS_SENTINEL, ('locations', 'service_name', 'db')))
+            **omit(settings.CACHEOPS_SENTINEL, ('locations', 'service_name', 'db')))
         return sentinel.master_for(
             settings.CACHEOPS_SENTINEL['service_name'],
             redis_class=client_class,
