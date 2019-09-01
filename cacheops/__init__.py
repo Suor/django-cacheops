bb25edb160d8425b0c0b3177418916f97dcff50f
Alexander Schepanovski <suor.web@gmail.com>
2019-09-01 14:05:03 +0700

    Up to 4.2

--- cacheops/__init__.py
+++ cacheops/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '4.1'
+__version__ = '4.2'
 VERSION = tuple(map(int, __version__.split('.')))
 
 
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = open('README.rst').read()    \
 
 setup(
     name='django-cacheops',
-    version='4.1',
+    version='4.2',
     author='Alexander Schepanovski',
     author_email='suor.web@gmail.com',
 
