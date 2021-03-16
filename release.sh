5dc257bc2778ab2dda2f0250d5a49e11bee74517
long.hoang <long.hoang@parcelperform.com>
2021-03-16 12:02:28 +0700

    Fix release.sh file

--- release.sh
+++ release.sh
@@ -4,9 +4,9 @@
 #rm dist/*
 #python setup.py bdist_wheel
 
-git clone git@git.parcelperform.com:dev-team/pp_django_cacheops.git pp_django_cacheops_clone
+git clone git@git.parcelperform.com:dev-team/pp_django_cacheops.git /pp_django_cacheops_clone
 echo "project cloned"
-cd pp_django_cacheops_clone
+cd /pp_django_cacheops_clone
 VERSION=`cat VERSION`
 output=$(git tag -a $VERSION -m $VERSION 2>&1)
 if [[ $output == *"fatal"* ]]; then
