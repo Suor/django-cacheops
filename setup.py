940cb0ea18ccaf8bc1204e94092735e14422f354
Alexander Schepanovski <suor.web@gmail.com>
2021-04-23 12:26:09 +0700

    Start testing Django 3.2, stop Django pre 3 and Python 3.5
    These will still work for a while.

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
 
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
