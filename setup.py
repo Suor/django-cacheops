50af4b4f0038f383883e4b8c43538d6a3411e3ab
Jair Henrique <jair.henrique@gmail.com>
2021-10-13 11:04:35 -0300

    Add support to python 3.10

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'cacheops.management.commands',
         'cacheops.templatetags'
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     install_requires=[
         'django>=2.1',
         'redis>=3.0.0',
@@ -37,11 +37,11 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Framework :: Django',
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
