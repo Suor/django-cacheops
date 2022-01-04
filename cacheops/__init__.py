c8931155ec53d018a8f2cd39018894eac00c12bf
Alexander Schepanovski <suor.web@gmail.com>
2022-01-04 15:55:39 +0700

    Fix linting

--- cacheops/__init__.py
+++ cacheops/__init__.py
@@ -2,10 +2,11 @@ __version__ = '5.0.1'
 VERSION = tuple(map(int, __version__.split('.')))
 
 import django
-from .simple import *
-from .query import *
-from .invalidation import *
-from .templatetags.cacheops import *
+from .simple import *  # noqa
+from .query import *  # noqa
+from .invalidation import *  # noqa
+from .templatetags.cacheops import *  # noqa
+
 
 if django.VERSION < (3, 2):
     default_app_config = "cacheops.apps.CacheopsConfig"
