a81288d6a37bd7dabc3592507ea4d2d8b27b04f2
Bruno Alla <alla.brunoo@gmail.com>
2022-07-11 10:12:18 +0100

    Expose reap_conjs on the package's top level API

--- cacheops/__init__.py
+++ cacheops/__init__.py
@@ -5,6 +5,7 @@ import django
 from .simple import *  # noqa
 from .query import *  # noqa
 from .invalidation import *  # noqa
+from .reaper import *  # noqa
 from .templatetags.cacheops import *  # noqa
 
 
